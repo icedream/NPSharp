39270c29926e0942caf373f50c8885e2cd06379d
icedream <icedream2k9@die-optimisten.net>
2014-05-22 17:08:19 +0200

    Added MasterServerMessage and attribute for it

---
+++ src/libnpsharp/Master/Messages/MasterServerMessageAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NPSharp.Master.Messages
+{
+    public class MasterServerMessageAttribute : Attribute
+    {
+        public MasterServerMessageAttribute(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+    }
+}
